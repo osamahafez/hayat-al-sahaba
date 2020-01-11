724c6425efcaf423ee3a6eb69bbfab59307e3eac
Osama Hafez <Eng.Osama.M.Hafez@gmail.com>
2020-01-11 14:46:46 +0200

    feat: add top ten sahaba

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,33 @@
 <template>
-  <header class="flex justify-around items-center mt-4">
+<main>
+
+  <section class="landing flex justify-around items-center mt-4">
     <div>
       <h1>السير فى خطى الصحابة</h1>
       <h1 class="text-orange-500">رضي الله عنهم</h1>
     </div>
     <img src="@/assets/images/logo.png" alt="hayat al-sahaba" />
-  </header>
+  </section>
+  <section class="top-ten mt-20">
+    <h1 class="text-orange-500">العشرة المبشرين بالجنة</h1>
+    <div class="flex justify-around">
+      <ul>
+        <li>1. أبو بكر الصديق</li>
+        <li>3. عثمان بن عفان</li>
+        <li>5. طلحة بن عبيد الله</li>
+        <li>7. عبد الرحمن بن عوف</li>
+        <li>9. سعد بن أبى وقاص</li>
+      </ul>
+      <ul>
+        <li>2. عمر بن الخطاب</li>
+        <li>4. على بن أبى طالب</li>
+        <li>6. الزبير بن العوام</li>
+        <li>7. سعيد بن زيد</li>
+        <li>10. أبو عبيدة بن الجراح</li>
+      </ul>
+    </div>
+  </section>
+</main>
 </template>
 
 <script>
@@ -22,7 +44,7 @@ export default {
 }
 */
 
-header { 
+.landing { 
   h1 {
     font-size: 70px;
     font-family: 'rawy';
@@ -35,6 +57,20 @@ header {
   }
 }
 
+.top-ten {
+  h1 {
+    font-size: 60px;
+    font-family: 'rawy';
+    text-align: center;
+  }
+  ul {
+    margin: 30px 0;
+    li {
+        font-size: 30px;
+        margin-top: 15px;
+    }
+  }
+}
 
 
 </style>

5e31827998d3828ecc4fafa4f412f2417dc5a4a6
Osama Hafez <Eng.Osama.M.Hafez@gmail.com>
2020-01-11 14:08:19 +0200

    feat: landing styling

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,11 @@
 <template>
-<div>
-  شبص
-</div>
+  <header class="flex justify-around items-center mt-4">
+    <div>
+      <h1>السير فى خطى الصحابة</h1>
+      <h1 class="text-orange-500">رضي الله عنهم</h1>
+    </div>
+    <img src="@/assets/images/logo.png" alt="hayat al-sahaba" />
+  </header>
 </template>
 
 <script>
@@ -17,5 +21,20 @@ export default {
   @apply min-h-screen flex justify-center items-center text-center mx-auto;
 }
 */
+
+header { 
+  h1 {
+    font-size: 70px;
+    font-family: 'rawy';
+    text-align: center;
+  }
+
+  img {
+    width: 500px;
+    height: 500px;
+  }
+}
+
+
 
 </style>
